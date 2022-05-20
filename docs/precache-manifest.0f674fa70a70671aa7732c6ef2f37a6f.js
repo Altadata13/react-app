1167f7030a394014dcd09a2269b61a25388680c0
Jose Caicedo <joseantoniocaicedo4@gmail.com>
2022-05-20 15:20:09 -0500

    app react

---
+++ docs/precache-manifest.0f674fa70a70671aa7732c6ef2f37a6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000c6f5e1c4cc91727613734d5f6a6bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "426f4db20be3852086fe",
+    "url": "/static/css/main.06100c70.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426f4db20be3852086fe",
+    "url": "/static/js/main.0ea9bffb.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
